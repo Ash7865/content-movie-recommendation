fecab1779b2c18848ae1d8dad55a87c3137611ba
Ash7865 <ashishku1920@gmail.com>
2026-01-17 02:34:48 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+## edit below variables as per your requirements -
+REPO_NAME = "content-movie-recommendation"
+AUTHOR_USER_NAME = "ASHISH KUMAR"
+LIST_OF_REQUIREMENTS = ['streamlit']
+
+
+setup(
+    name=ASH_REPO,
+    version="0.0.1",
+    author=AUTHOR_USER_NAME,
+    description="A small package for Movie Recommender System",
+    url=f"https://github.com/Ash7865/content-movie-recommendation.git/{ashish kumar}/{content-movie-recommendation}",
+    author_email="ashishku1920@gmail.com",
+    packages=[ASH_REPO],
+    python_requires=">=3.7",
+    install_requires=LIST_OF_REQUIREMENTS
+)
